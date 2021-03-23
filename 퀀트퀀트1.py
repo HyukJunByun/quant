a19da30a9e8eff7a6f27ee50bfc9a673a261e5f1
HyukJunByun <tim2425@yonsei.ac.kr>
2021-03-23 12:33:45 +0900

    kind에서 모든 법인 종목코드 가져오기 완료

--- 퀀트퀀트1.py
+++ 퀀트퀀트1.py
@@ -4,15 +4,14 @@ from bs4 import BeautifulSoup
 
 code_data = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13', header=0)[0]
 
-code_data = code_data['종목코드']
-# print(code_data)
-#['종목코드'] 요것만 하면 판다스에 종목코드가 리스트로 저장될까?
-
 def make_code(x):
     x = str(x)
     return '0' * (6 - len(x)) + x
 
-code_data['종목코드'] = code_data['종목코드'].apply(make_code)
+code_data = code_data['종목코드']
+#['종목코드'] 요것만 하면 판다스에 종목코드가 리스트로 저장될까? ㅇㅇ 가능하네
+code_data = code_data.apply(make_code)
+#print(code_data)
 
-webpage = requests.get("http://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&gicode=A005930&cID=&MenuYn=Y&ReportGB=D&NewMenuID=Y&stkGb=701")
+#webpage = requests.get("http://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&gicode=A005930&cID=&MenuYn=Y&ReportGB=D&NewMenuID=Y&stkGb=701")
 # print(webpage.text)
