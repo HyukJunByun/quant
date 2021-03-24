14d0f97fae77e56156d88aa6b2ce0ef5f6bda6f6
Hyuk Jun Byun <tim2425@yonsei.ac.kr>
2021-03-24 16:47:11 +0900

    html 정보 가져올때 {}없이 태그 하나만 find 할수 있는지 테스트

--- 퀀트퀀트1.py
+++ 퀀트퀀트1.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from openpyxl import Workbook
 
 code_data = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13', header=0)[0]
 
@@ -15,11 +16,17 @@ code_data = code_data.apply(make_code)
 
 code_num = code_data[1571]
 #(임시 테스트)제일기획 종목코드 가져오기
+
 fnguide_url = "http://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&gicode=A" + code_num + "&cID=&MenuYn=Y&ReportGB=D&NewMenuID=Y&stkGb=701"
+#종목코드를 이용해서 fnquide 접속
 webpage = requests.get(fnguide_url)
 web_data = BeautifulSoup(webpage.content, 'html.parser')
-target = web_data.find('div', {'class': 'um_table', 'id': 'highlight_D_A'})
-print(target)
-# print(webpage.text)
+#웹페이지 정보 가져오기
+corp_name = web_data.find('h1', 'id': 'giName')
+#주식이름
+ifrs_D_A = web_data.find('div', {'class': 'um_table', 'id': 'highlight_D_A'})
+#ifrs표 데이터 불러오기
+corp_name = web_data.find('h1', 'id': 'giName')
 
-#<table class="us_table_ty1 h_fix zigbg_no">
+
+# print(webpage.text)
