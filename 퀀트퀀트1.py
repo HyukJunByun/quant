36b86d3a9e0457db5ec2ce0283c441fae2178248
Hyuk Jun Byun <tim2425@yonsei.ac.kr>
2021-03-24 17:13:40 +0900

    시세현황표 안에 숫자들 불러오기 테스트

--- 퀀트퀀트1.py
+++ 퀀트퀀트1.py
@@ -22,11 +22,15 @@ webpage = requests.get(fnguide_url)
 web_data = BeautifulSoup(webpage.content, 'html.parser')
 #웹페이지 정보 가져오기
 corp_name = web_data.find('h1', {'id': 'giName'})
-print(corp_name)
-#주식이름, 태그 하나만 가져와도 {}를 붙여야하네
+#주식이름            +++태그 하나만 가져와도 {}를 붙여야하네, 참고로 이거는 태그까지 전부 포함한 데이터라 .text로 문자만 뽑아내야함
+price_and_num = web_data.find('div', {'class': 'um_table', 'id': 'svdMainGrid1'})
+#시세현황표 
+price_and_num_data = price_and_num.find_all('td')
+#시세현황표 안에 있는 숫자들 전부 가져오기
+print(price_and_num_data.text)
 #ifrs_D_A = web_data.find('div', {'class': 'um_table', 'id': 'highlight_D_A'})
 #ifrs표 데이터 불러오기
-#corp_name = web_data.find('h1', 'id': 'giName')
+
 
 
 # print(webpage.text)
