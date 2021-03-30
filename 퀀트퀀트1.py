3b22a0f8b9dc62fb6b30ec776abd5ffa518a1d19
Hyuk Jun Byun <tim2425@yonsei.ac.kr>
2021-03-30 20:41:16 +0900

    numpy dtype 계산식 테스트 완료

--- 퀀트퀀트1.py
+++ 퀀트퀀트1.py
@@ -93,7 +93,7 @@ for a in range(0, 1):
                     DQ.columns = column
                     BQ.columns = column
                     for i in column:
-                        DA[i] = DA[i].apply(pd.to_numeric, errors = 'coerce')
+                        DA[i] = DA[i].apply(pd.to_numeric, errors = 'ignore')
                         DQ[i] = DQ[i].apply(pd.to_numeric, errors = 'ignore')
                         BQ[i] = BQ[i].apply(pd.to_numeric, errors = 'ignore')
                     price_and_num = pd.read_html(fnguide_url, match='시세현황', flavor='lxml', index_col=0, attrs={'class': 'us_table_ty1 table-hb thbg_g h_fix zigbg_no'})[0]
@@ -115,7 +115,7 @@ for a in range(0, 1):
                         # 1/3/6/12개월 수익률
                         all_zoo = pd.to_numeric(price_and_num[1]['발행주식수(보통주/ 우선주)'], errors = 'ignore')
                         # 발행주식수
-                        my_zoo = pd.to_numeric(my_zoo_table['보통주']['자기주식 (자사주+자사주신탁)'], errors = 'ignore')
+                        my_zoo = pd.to_numeric(my_zoo_table.loc['자기주식 (자사주+자사주신탁)', '보통주'], errors = 'ignore')
                         # 자기주식수
                         my_zoo_table = None
                         price_and_num = None
@@ -123,13 +123,13 @@ for a in range(0, 1):
                         # 재무제표
                         recent_DA = DA.iloc[0, 4] # 최신 사업보고서 (연도/12월) str
                         earn = pd.read_html(fnguide_url, match='포괄손익계산서', flavor='lxml', header=0, index_col=0, attrs={'class': 'us_table_ty1 h_fix zigbg_no'})[0]
-                        earn_all = pd.to_numeric(earn[recent_DA]['매출총이익'], errors = 'ignore')
+                        earn_all = pd.to_numeric(earn.loc['매출총이익', recent_DA], errors = 'ignore')
                         # 매출총이익
                         earn = None
                         gpa = earn_all / DA[4]['자산총계']
                         # GP/A
                         cash_flow_table = pd.read_html(fnguide_url, match='현금흐름표', flavor='lxml', header=0, index_col=0, attrs={'class': 'us_table_ty1 h_fix zigbg_no'})[0]
-                        cash_flow = pd.to_numeric(cash_flow_table[recent_DA]['영업활동으로인한현금흐름'], errors = 'ignore')
+                        cash_flow = pd.to_numeric(cash_flow_table.loc['영업활동으로인한현금흐름', recent_DA], errors = 'ignore')
                         # 영업현금흐름
                         pcr = siga / cash_flow
                         # PCR
@@ -137,7 +137,7 @@ for a in range(0, 1):
                         # 영업이익
                         kakao = DA[4]['당기순이익']
                         # 당기순이익(지배+비지배)
-                        apple = pd.to_numeric(cash_flow_table[recent_DA]['유상증자'], errors = 'ignore')
+                        apple = pd.to_numeric(cash_flow_table.loc['유상증자', recent_DA], errors = 'ignore')
                         # 유상증자
                         cash_flow_table = None
                         if(pcr > 1 and  naver > 0 and cash_flow > 0 and kakao > 0 and math.isnan(apple) == True):
@@ -145,7 +145,7 @@ for a in range(0, 1):
                             fnguide_url = 'http://comp.fnguide.com/SVO2/ASP/SVD_FinanceRatio.asp?pGB=1&gicode=A' + code_num + '&cID=&MenuYn=Y&ReportGB=&NewMenuID=104&stkGb=701'
                             # 재무비율
                             mon_ratio = pd.read_html(fnguide_url, match='재무비율', flavor='lxml', header=0, index_col=0, attrs={'class': 'us_table_ty1 h_fix zigbg_no'})[0]
-                            owe = pd.to_numeric(mon_ratio[recent_DA]['순차입금비율계산에 참여한 계정 펼치기'], errors = 'ignore')
+                            owe = pd.to_numeric(mon_ratio.loc['순차입금비율계산에 참여한 계정 펼치기', recent_DA], errors = 'ignore')
                             # 순차입금비율
                             mon_ratio = None
                             if(math.isnan(owe) == True or owe < 200):
@@ -153,11 +153,11 @@ for a in range(0, 1):
                                 fnguide_url = 'http://comp.fnguide.com/SVO2/ASP/SVD_Invest.asp?pGB=1&gicode=A' + code_num + '&cID=&MenuYn=Y&ReportGB=&NewMenuID=105&stkGb=701'
                                 # 투자지표
                                 invest_idea = pd.read_html(fnguide_url, match='기업가치 지표', flavor='lxml', header=0, index_col=0, attrs={'class': 'us_table_ty1 h_fix zigbg_no'})[0]
-                                bay_trend = pd.to_numeric(invest_idea[recent_DA]['배당성향(현금)(%)계산에 참여한 계정 펼치기'], errors = 'ignore')
+                                bay_trend = pd.to_numeric(invest_idea.loc['배당성향(현금)(%)계산에 참여한 계정 펼치기', recent_DA], errors = 'ignore')
                                 # 배당성향
-                                ev_ebita = pd.to_numeric(invest_idea[recent_DA]['EV/EBITDA계산에 참여한 계정 펼치기'], errors = 'ignore')
+                                ev_ebita = pd.to_numeric(invest_idea.loc['EV/EBITDA계산에 참여한 계정 펼치기', recent_DA], errors = 'ignore')
                                 # EV/EBITA
-                                fcff = pd.to_numeric(invest_idea[recent_DA]['FCFF'], errors = 'ignore')
+                                fcff = pd.to_numeric(invest_idea.loc['FCFF', recent_DA], errors = 'ignore')
                                 pfcr = siga / fcff
                                 invest_idea = None
                                 # PFCR
